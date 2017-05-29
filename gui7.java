560b4aee4239c3c3274629bd3486278e971198ec
guptasaloni <salonigupta.8330@gmail.com>
2017-05-29 10:11:51 +0530

    my gui application

---
+++ gui7.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+//import javax.swing.*;
+
+class gui7
+{
+	public static void main(String args[])
+	{
+		Frame f=new Frame("Scrollbar");
+		Panel p=new Panel();
+		f.setSize(200,200);
+		f.setLayout(new FlowLayout(2,0,0));
+		Scrollbar sc1=new Scrollbar();
+		//sc1.setMaximum(300);
+		//sc1.setMinimum(0);
+		//sc1.setValue(0);
+		sc1.setBackground(Color.red);
+		Scrollbar sc2=new Scrollbar();
+		//sc2.setMaximum(300);
+		//sc2.setMinimum(0);
+		//sc2.setValue(3);
+		sc2.setBackground(Color.green);
+		//sc1.setLayout(new BorderLayout());
+		//p.add(sc1,BorderLayout.EAST);
+		//p.add(sc2,BorderLayout.EAST);
+		f.add(sc1);
+		f.add(sc2);
+		f.setVisible(true);
+	}
+}
